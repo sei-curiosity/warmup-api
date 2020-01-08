ac36bbb592366648d61a759395d689029b09b746
samidahlawi <o.t5@hotmail.com>
2020-01-08 11:14:17 +0300

    add the solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 
 class App extends Component {
@@ -30,11 +31,31 @@ class App extends Component {
   handleSubmit = (event) => {
     // not reload the page
     // prevent default action
-    
+     event.preventDefault()
     // get the formData from state
-    
-    // copy of the images state 
-    
+    const copyFormData = {...this.state.formData}
+    /// take the input of form 
+    const input = copyFormData.name;
+    // Axios
+    axios.get(`https://api.giphy.com/v1/gifs/search?api_key=CLNh2lk5k8r0D3gH7L0xrYlv4ktLBUcM&q=${input}&limit=1&offset=0&rating=G&lang=en`)
+    .then(response => {
+      const imageId = response.data.data[0].id
+      const url = response.data.data[0].images.fixed_height.url
+      const image = {
+        id: imageId,
+        imageUrl: url
+      }
+      // copy of the images state 
+      const copyImages = [...this.state.images]
+      // const copyIamges = this.state.images.slice()
+      copyImages.push(image)
+      // [{id:'3323dsf',imageUrl:'http://'}]
+      this.setState({
+        images:copyImages
+      })
+     
+    })
+    .catch(error => (console.log(error)))
     //++ push our formData into the images copy
     
   }
@@ -42,9 +63,13 @@ class App extends Component {
  /// function for delete the element from the array by use the id
   deleteImages = (id) =>{
    // copy the images from state 
- 
+    let copyImages = [...this.state.images]
+    copyImages = copyImages.filter((image) => (image.id != id))
+
    // filter the array of images then remove the movie which has the same 'id' 
-   
+   this.setState({
+     images:copyImages
+   })
 
    
   }
@@ -56,19 +81,27 @@ class App extends Component {
         <form  onSubmit={this.handleSubmit}>
           
           <label>Name: </label>
-            <input 
-            name="name"
-            value={this.state.formData.name}
-            placeholder="Enter your Name"
-            onChange={this.handleChange}
-            ></input>
+              <input 
+              name="name"
+              value={this.state.formData.name}
+              placeholder="Enter your Name"
+              onChange={this.handleChange}
+              ></input>
           <br/>
-
+          
           <button type="submit">Submit</button>
         </form>
 
 
-
+        {this.state.images.map( (image,index) => (
+          <div key={index}>
+            <img src={image.imageUrl} alt= "" ></img>
+            <br/>
+        <button onClick={() => this.deleteImages(image.id)}>Delete</button>
+            <hr/>
+            </div>
+        ) )}
+      
 
         
        
